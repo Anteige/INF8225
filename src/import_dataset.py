0efe1416ebdd3ebaf0f1e1a24707c91b910b9720
AnisRedjdal <nissow301@gmail.com>
2020-04-20 23:56:55 -0400

    avancement model

--- src/import_dataset.py
+++ src/import_dataset.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 from skimage.io import imread
 
-def loadDataset(path):
+def loadDataset2D(path):
     datasetX = []
     datasetY = []
     i = 0
@@ -17,7 +17,27 @@ def loadDataset(path):
                 X = X.reshape(X_size)
                 datasetX.append(X)
                 datasetY.append(folder.split("-")[1])
-                if i == 100: 
+#                 if i == 100: 
+#                     #print("fin de la recuperation des images")
+#                     break
+#                 i += 1
+
+    return np.array(datasetX), np.array(datasetY)
+
+def loadDataset4D(path):
+    datasetX = []
+    datasetY = []
+    i = 0
+    print("debut chargement image")
+    for folder in os.listdir(path):
+        for image in os.listdir(path + "/" + folder):
+            if (image.endswith('.jpg')):
+                X = imread(path + "/" + folder+ "/" + image)[:,:,0]#[...,::-1]#
+                new_dim = []
+                new_dim.append(X)
+                datasetX.append(new_dim)
+                datasetY.append(folder.split("-")[1])
+                if i == 200: 
                     #print("fin de la recuperation des images")
                     break
                 i += 1
