77cb515f529feb375d6c4d96f25911df0d13fb74
AnisRedjdal <nissow301@gmail.com>
2020-04-21 14:05:18 -0400

    ajoute NetModel probleme de CPU

--- src/import_dataset.py
+++ src/import_dataset.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 from skimage.io import imread
 
-def loadDataset2D(path):
+def loadDataset(path):
     datasetX = []
     datasetY = []
     i = 0
@@ -11,16 +11,9 @@ def loadDataset2D(path):
     for folder in os.listdir(path):
         for image in os.listdir(path + "/" + folder):
             if (image.endswith('.jpg')):
-                X = imread(path + "/" + folder+ "/" + image)#[:,:,0]#[...,::-1]
-                rows,cols,colors = X.shape
-                X_size = rows*cols*colors
-                X = X.reshape(X_size)
+                X = imread(path + "/" + folder+ "/" + image)[:,:,0]#[...,::-1]
                 datasetX.append(X)
                 datasetY.append(folder.split("-")[1])
-#                 if i == 100: 
-#                     #print("fin de la recuperation des images")
-#                     break
-#                 i += 1
 
     return np.array(datasetX), np.array(datasetY)
 
