5c3a068d6be745e7a6893b56759faaddeb98e13a
AnisRedjdal <nissow301@gmail.com>
2020-04-23 23:32:37 -0400

    clean model

--- src/import_dataset.py
+++ src/import_dataset.py
@@ -3,6 +3,20 @@ import matplotlib.pyplot as plt
 import os
 from skimage.io import imread
 
+# def loadDataset(path):
+#     datasetX = []
+#     datasetY = []
+#     i = 0
+#     print("debut chargement image")
+#     for folder in os.listdir(path):
+#         for image in os.listdir(path + "/" + folder):
+#             if (image.endswith('.jpg')):
+#                 X = imread(path + "/" + folder+ "/" + image)[:,:,0]#[...,::-1]
+#                 datasetX.append(X)
+#                 datasetY.append(folder.split("-")[1])
+
+#     return np.array(datasetX), np.array(datasetY)
+
 def loadDataset(path):
     datasetX = []
     datasetY = []
@@ -11,24 +25,9 @@ def loadDataset(path):
     for folder in os.listdir(path):
         for image in os.listdir(path + "/" + folder):
             if (image.endswith('.jpg')):
-                X = imread(path + "/" + folder+ "/" + image)[:,:,0]#[...,::-1]
+                X = imread(path + "/" + folder+ "/" + image)#[:,:,0]#[...,::-1]#
+                X = X.reshape(3, 224, 224)
                 datasetX.append(X)
-                datasetY.append(folder.split("-")[1])
-
-    return np.array(datasetX), np.array(datasetY)
-
-def loadDataset4D(path):
-    datasetX = []
-    datasetY = []
-    i = 0
-    print("debut chargement image")
-    for folder in os.listdir(path):
-        for image in os.listdir(path + "/" + folder):
-            if (image.endswith('.jpg')):
-                X = imread(path + "/" + folder+ "/" + image)[:,:,0]#[...,::-1]#
-                new_dim = []
-                new_dim.append(X)
-                datasetX.append(new_dim)
                 datasetY.append(folder.split("-")[1])
                 if i == 200: 
                     #print("fin de la recuperation des images")
